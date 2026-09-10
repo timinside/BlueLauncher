3fc6319c439d2c0dfaac4fdafd95002a8f23434e
bbzer0 <timkainside@gmail.com>
2026-09-10 09:22:00 +0300

    Add files via upload

---
+++ src/app/forms/MainForm.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class MainForm extends AbstractForm
+{
+
+    /**
+     * @event show 
+     */
+    function doShow(UXWindowEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button.click-Left 
+     */
+    function doButtonClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+}
---
+++ src/app/forms/Select.php
@@ -0,0 +1,74 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class Select extends AbstractForm
+{
+
+
+    /**
+     * @event button.click-Left 
+     */
+    function doButtonClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+
+
+
+
+    /**
+     * @event close 
+     */
+    function doClose(UXWindowEvent $e = null)
+    {    
+        
+    }
+
+
+    /**
+     * @event button6.click-Left 
+     */
+    function doButton6ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+
+
+    /**
+     * @event button9.click-Left 
+     */
+    function doButton9ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button10.click-Left 
+     */
+    function doButton10ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event buttonAlt.click-Left 
+     */
+    function doButtonAltClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button3.click-Left 
+     */
+    function doButton3ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+}
